8f337d5413b1f478e3c504c170e207ffc8231d97
eliethp11 <150207533+eliethp11@users.noreply.github.com>
2026-05-16 11:09:46 -0500

    procesador dicom

--- procesador_dicom.py
+++ procesador_dicom.py
@@ -5,3 +5,19 @@ import pandas as pd
 import cv2
 import os
 
+def __init__(self):
+    self.archivos_dicom=[]
+    self.dataframe=None
+
+def cargar_archivos (self,directorio):
+    self.archivos_dicom=[]
+    for archivo in os.listdir(directorio):
+        ruta=os.path.join (directorio,archivo)
+        try:
+            ds=pydicom.dcmread(ruta)
+            self.archivos_dicom.append (ds)
+            print(f"Cargado:{archivo}")
+        except Exception as e:
+            print(f"El DICOM no es válido:{archivo}---{e}")
+
+        print(f"\nTotal de archivos cargados:{len(self.archivos_dicom)}")
